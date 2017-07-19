d424d4784bf6dd76046e2e16bed97d1d5b8e08a1
RamDie <dievaldi@gmail.com>
2017-07-19 19:52:14 -0300

    date added

--- gitTest/src/gitTest/Init.java
+++ gitTest/src/gitTest/Init.java
@@ -1,10 +1,18 @@
 package gitTest;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 public class Init {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Yellow Git");
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy MMM dd HH:mm:ss");
+		Calendar calendar = new GregorianCalendar(2013,0,31);		
+		
+		System.out.println("Yellow Git " + sdf.format(calendar.getTime()));
+
 	}
 
 }
